24ec6c78ffa73633cd0b1f15eecc01447d225e9d
kiran04-code <kr551344@gmail.com>
2025-09-03 01:07:45 +0530

    indeployment mode4

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(Auth("UserToken"))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173 ",'https://highwaydelite-1-xdcl.onrender.com'],
     credentials: true, // if you're sending cookies or auth headers
 }));
 // Mongose Coonection
